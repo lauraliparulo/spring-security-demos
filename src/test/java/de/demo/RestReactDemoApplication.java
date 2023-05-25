f4ba817d63fae5bbdbf937afea4ebe6541f6d89d
Liparulo <laura.liparulo@capgemini.com>
2023-05-25 22:04:31 +0200

    ref

--- src/test/java/de/demo/RestReactDemoApplication.java
+++ src/test/java/de/demo/RestReactDemoApplication.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Chapter2ApplicationTests {
+class RestReactDemoApplication {
 
   @Test
   void contextLoads() {}
