1fe4061c12ddc955c47c5398bb608dc24ef31371
Laura Liparulo <lauratailz@yahoo.it>
2023-09-11 17:05:28 +0200

    ldap auth added!

--- src/main/java/de/demo/config/UserManagementSecurityConfig.java
+++ src/main/java/de/demo/config/UserManagementSecurityConfig.java
@@ -1,56 +1,32 @@
 package de.demo.config;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import de.demo.entity.user.UserAccount;
-import de.demo.repo.UserAccountRepository;
+import org.springframework.security.ldap.DefaultLdapUsernameToDnMapper;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+import org.springframework.security.ldap.userdetails.LdapUserDetailsManager;
 
 @Configuration
 public class UserManagementSecurityConfig {
 
-	@Bean
-	CommandLineRunner initUsers(UserAccountRepository repository) {
-		return args -> {
-			repository.save(new UserAccount("alice", "1234", "USER"));
-			repository.save(new UserAccount("bob", "1234", "ROLE_USER"));
-			repository.save(new UserAccount("admin", "1234", "ADMIN"));
-		};
-	}
+	 @Bean                                                       
+	  public UserDetailsService userDetailsService() {
+	    var cs = new DefaultSpringSecurityContextSource(          
+	      "ldap://127.0.0.1:33389/dc=springframework,dc=org");
+	    cs.afterPropertiesSet();
 
+	    var manager = new LdapUserDetailsManager(cs);             
 
-	  @Bean
-	  public UserDetailsService inMemoryUserDetailsService(UserAccountRepository repo) {
-		  
-		    UserDetails user =
-		            User.builder()
-		               .username("alice")
-		               .password(passwordEncoder().encode("1234"))
-		               .authorities("read")
-		               .roles("USER")
-		               .build();
-		    
-		    UserDetails admin =
-		            User.builder()
-		               .username("admin")
-		               .password(passwordEncoder().encode("1234"))
-		               .roles("ADMIN")
-		               .build();
-		    
-		    //TODO Database add
-		    
-		    return new InMemoryUserDetailsManager(user,admin);
-		  
-		  
+	    manager.setUsernameMapper(                                
+	      new DefaultLdapUsernameToDnMapper("ou=groups", "uid"));
+
+	    manager.setGroupSearchBase("ou=groups");                  
+	    
+	    return manager;    
 	  }
-	  
-	  
+	   
 	  //TODO UsernamePasswordAuthenticationToken
 	  
 	    @Bean
