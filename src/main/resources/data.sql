52f7f703874f10281d79a7b7fffca6ebfac6f81b
Laura Liparulo <lauratailz@yahoo.it>
2023-09-11 12:12:35 +0200

    sql scripts added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO `spring`.`authorities` VALUES (NULL, 'admin', 'write');
+INSERT INTO `spring`.`authorities` VALUES (NULL, 'admin', 'read');
+INSERT INTO `spring`.`authorities` VALUES (NULL, 'admin', 'delete');
+INSERT INTO `spring`.`authorities` VALUES (NULL, 'alice', 'write');
+INSERT INTO `spring`.`authorities` VALUES (NULL, 'alice', 'read');
+INSERT INTO `spring`.`users` VALUES (NULL, 'alice', '1234', '1');
+INSERT INTO `spring`.`users` VALUES (NULL, 'admin', '1234', '1');
