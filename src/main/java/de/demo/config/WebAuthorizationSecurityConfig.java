f2d5a9337edff56034b2933d722dfe9a2cb7fe5c
Laura Liparulo <lauratailz@yahoo.it>
2023-09-12 17:34:02 +0200

    handlers added

--- src/main/java/de/demo/config/WebAuthorizationSecurityConfig.java
+++ src/main/java/de/demo/config/WebAuthorizationSecurityConfig.java
@@ -2,6 +2,7 @@ package de.demo.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,52 +10,47 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import de.demo.handlers.CustomAuthenticationFailureHandler;
+import de.demo.handlers.CustomAuthenticationSuccessHandler;
+
+
 @Configuration
 @EnableMethodSecurity
 //@EnableWebSecurity
 public class WebAuthorizationSecurityConfig {
+	
+    @Autowired
+    private CustomAuthenticationSuccessHandler authenticationSuccessHandler;
 
-	
-//		@Bean
-//		@Order(1)                                                        
-//		public SecurityFilterChain apiFilterChain(HttpSecurity httpSecurity) throws Exception {
-//			httpSecurity
-//				.securityMatcher("/api/**")                                   
-//				.authorizeHttpRequests(authorize -> authorize
-//					.anyRequest().hasRole("ADMIN")
-//				)
-//				.httpBasic(withDefaults());
-//			return httpSecurity.build();
-//		}
-	
-		
-	  @Bean
-	  
-	  SecurityFilterChain formLoginFilterChain(HttpSecurity httpSecurity) throws Exception {
-		  
-		  httpSecurity.httpBasic(c -> {
-	            c.realmName("OTHER");
-	            c.authenticationEntryPoint(new CustomEntryPoint());
-	        });
-		     	        	 
-		  httpSecurity.authorizeHttpRequests() //
+    @Autowired
+    private CustomAuthenticationFailureHandler authenticationFailureHandler;
+
+	@Bean
+	SecurityFilterChain formLoginFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+		httpSecurity.formLogin().successHandler(authenticationSuccessHandler)
+				.failureHandler(authenticationFailureHandler).and().httpBasic(c -> {
+					c.realmName("OTHER");
+					c.authenticationEntryPoint(new CustomEntryPoint());
+				});
+
+		httpSecurity.authorizeHttpRequests() //
 //          .requestMatchers("/").authenticated()
-          .requestMatchers("/login").permitAll()
-          .requestMatchers(HttpMethod.POST, "/search").authenticated() //
-          .requestMatchers(HttpMethod.GET, "/api/**").authenticated()//
-          .requestMatchers("/admin").hasRole("ADMIN") //
-          .requestMatchers("/h2-console").hasRole("ADMIN") //
-          .requestMatchers(HttpMethod.POST, "/delete/**", "/new-video").authenticated() //
-          .anyRequest().authenticated() //
-	              .and() //
+				.requestMatchers("/login").permitAll().requestMatchers(HttpMethod.POST, "/search").authenticated() //
+				.requestMatchers(HttpMethod.GET, "/api/**").authenticated()//
+				.requestMatchers("/admin").hasRole("ADMIN") //
+				.requestMatchers("/h2-console").hasRole("ADMIN") //
+				.requestMatchers(HttpMethod.POST, "/delete/**", "/new-video").authenticated() //
+				.anyRequest().authenticated() //
+				.and() //
 //	              .exceptionHandling().accessDeniedHandler(accessDeniedHandler())
-	              .formLogin(withDefaults()) //
-	              .httpBasic(withDefaults());
-	    return httpSecurity.build();
-	  }
-	  
-	  
-//TODO	   @Bean
+				.formLogin(withDefaults()) //
+				.httpBasic(withDefaults());
+		return httpSecurity.build();
+
+	}
+
+//TODO	@Bean
 //	    public AccessDeniedHandler accessDeniedHandler() {
 //	        return new CustomAccessDeniedHandler();
 //	    }
---
+++ src/main/java/de/demo/handlers/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,19 @@
+package de.demo.handlers;
+
+import java.time.LocalDateTime;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e)  {
+        httpServletResponse.setHeader("failed", LocalDateTime.now().toString());
+    }
+}
