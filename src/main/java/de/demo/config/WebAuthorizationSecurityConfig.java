6b3eec7dcca783b9313e44122a770c349847175e
Liparulo <laura.liparulo@capgemini.com>
2023-09-20 17:53:17 +0200

    github oauth login

--- src/main/java/de/demo/config/WebAuthorizationSecurityConfig.java
+++ src/main/java/de/demo/config/WebAuthorizationSecurityConfig.java
@@ -2,54 +2,71 @@ package de.demo.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import de.demo.filters.AuthenticationLoggingFilter;
+import de.demo.filters.CsrfTokenLogger;
+import de.demo.filters.RequestValidationFilter;
+import de.demo.handlers.CustomAuthenticationFailureHandler;
+import de.demo.handlers.CustomAuthenticationSuccessHandler;
 
 @Configuration
 @EnableMethodSecurity
-//@EnableWebSecurity
+@EnableWebSecurity
 public class WebAuthorizationSecurityConfig {
 
-	
-//		@Bean
-//		@Order(1)                                                        
-//		public SecurityFilterChain apiFilterChain(HttpSecurity httpSecurity) throws Exception {
-//			httpSecurity
-//				.securityMatcher("/api/**")                                   
-//				.authorizeHttpRequests(authorize -> authorize
-//					.anyRequest().hasRole("ADMIN")
-//				)
-//				.httpBasic(withDefaults());
-//			return httpSecurity.build();
-//		}
-		
-	  @Bean
-	  SecurityFilterChain formLoginFilterChain(HttpSecurity httpSecurity) throws Exception {
-		     	        	 
-		  httpSecurity.authorizeHttpRequests() //
-//          .requestMatchers("/").authenticated()
-          .requestMatchers("/login").permitAll()
-          .requestMatchers(HttpMethod.POST, "/search").authenticated() //
-          .requestMatchers(HttpMethod.GET, "/api/**").authenticated()//
-          .requestMatchers("/admin").hasRole("ADMIN") //
-          .requestMatchers("/h2-console").hasRole("ADMIN") //
-          .requestMatchers(HttpMethod.POST, "/delete/**", "/new-video").authenticated() //
-          .anyRequest().authenticated() //
-	              .and() //
-//	              .exceptionHandling().accessDeniedHandler(accessDeniedHandler())
-	              .formLogin(withDefaults()) //
-	              .httpBasic(withDefaults());
-	    return httpSecurity.build();
-	  }
-	  
-	  
-//TODO	   @Bean
+	@Bean(name = "securityFilterChain")
+	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+		// TODO Not working!!!!
+		httpSecurity = httpSecurity.csrf(c -> {
+			c.ignoringRequestMatchers("/h2-console/**");
+		});
+
+		httpSecurity = httpSecurity.cors(withDefaults());
+
+		httpSecurity.oauth2Login();
+
+		httpSecurity.authorizeHttpRequests().requestMatchers("/login").permitAll().requestMatchers("/logout")
+				.permitAll().requestMatchers(HttpMethod.POST, "/search").authenticated() //
+				.requestMatchers(HttpMethod.GET, "/api/**").authenticated()//
+				.requestMatchers("/admin").hasRole("ADMIN") //
+				.requestMatchers("/h2-console/**").hasRole("ADMIN")
+				.requestMatchers(HttpMethod.POST, "/delete/**", "/new-video").authenticated()
+				//
+				.anyRequest().authenticated(); //
+
+		return httpSecurity.build();
+
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("https://example.com"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
+}
+
+//TODO	@Bean
 //	    public AccessDeniedHandler accessDeniedHandler() {
 //	        return new CustomAccessDeniedHandler();
 //	    }
-
-}
